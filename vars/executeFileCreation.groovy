ecf175ee72d865bf155b50ff35f60c3540c57aca
NidhiThakkar01 <144941514+NidhiThakkar01@users.noreply.github.com>
2023-09-18 13:41:57 +0530

    Update executeFileCreation.groovy

--- vars/executeFileCreation.groovy
+++ vars/executeFileCreation.groovy
@@ -1,5 +1,7 @@
 def call(def filename,def fileContent = "Hello, It is sample file for execution"){
   sh '''
+  echo ${filename}
+  echo $filename
   touch ${filename}
   writeFile(${filename}, fileContent)
   echo "File is written"
