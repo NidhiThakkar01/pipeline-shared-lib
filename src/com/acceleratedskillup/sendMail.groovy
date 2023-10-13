0c870ae7ffc4cad18047fa1b93955a60cf442bb1
NidhiThakkar01 <144941514+NidhiThakkar01@users.noreply.github.com>
2023-10-13 16:19:54 +0530

    Create sendMail.groovy

---
+++ src/com/acceleratedskillup/sendMail.groovy
@@ -0,0 +1,13 @@
+import hudson.model.AbstractProject
+import hudson.tasks.Mailer
+import hudson.model.User
+
+def printMailInfo(){
+  def item = hudson.model.Hudson.instance.getItem(env.JOB_NAME) 
+  def build = item.getLastBuild()
+  def cause = build.getCause(hudson.model.Cause.UserIdCause.class)
+  def id = cause.getUserId()
+  User u = User.get(id)
+  def umail = u.getProperty(Mailer.UserProperty.class)
+  print umail.getAddress()
+}
